3cd318a0e1bd597643d23a63be170312639b23cf
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-09-17 01:14:48 +0300

    When disabling channel, clear its timer_must_readd_encoding_irq

--- drivers/staging/media/tw5864/tw5864-video.c
+++ drivers/staging/media/tw5864/tw5864-video.c
@@ -212,6 +212,7 @@ static int tw5864_disable_input(struct tw5864_dev *dev, int input_number) {
 	tw_clearl(TW5864_PCI_INTR_CTL, TW5864_PCI_MAST_ENB | TW5864_MVD_VLC_MAST_ENB);
 	spin_lock_irqsave(&dev->slock, flags);
 	dev->inputs[input_number].enabled = 0;
+	dev->inputs[input_number].timer_must_readd_encoding_irq = 0;
 	dev->irqmask &= ~TW5864_INTR_VLC_DONE;  // timer doesn't like to get turned off?
 	tw5864_irqmask_apply(dev);
 	spin_unlock_irqrestore(&dev->slock, flags);
