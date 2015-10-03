ba51282833357ebe8ebb03884d0d1e908699a59b
Andrey Utkin <andrey.krieger.utkin@gmail.com>
2015-10-03 10:45:14 +0300

    Write forgotten INTRA_EN flag to reg on start

--- drivers/staging/media/tw5864/tw5864-video.c
+++ drivers/staging/media/tw5864/tw5864-video.c
@@ -289,6 +289,8 @@ int tw5864_enable_input(struct tw5864_dev *dev, int input_number) {
 	tw5864_irqmask_apply(dev);
 	spin_unlock_irqrestore(&dev->slock, flags);
 
+	tw_writel(TW5864_MOTION_SEARCH_ETC, TW5864_INTRA_EN); // produce intra frame
+
 	tw5864_push_to_make_it_roll(input);
 	tw5864_request_encoded_frame(input);
 
