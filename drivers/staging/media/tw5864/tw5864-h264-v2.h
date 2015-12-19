e02b861480ce904a4a5bc7e1b2e42cc0cb4d94b9
Andrey Utkin <andrey.od.utkin@gmail.com>
2015-12-19 02:28:21 +0200

    h264 files cleanup

--- drivers/staging/media/tw5864/tw5864-h264-v2.h
+++
@@ -1,3 +0,0 @@
-int h264_gen_sps_rbsp(void *p_data);
-int h264_gen_pps_rbsp(void *p_data);
-int h264_gen_slice_head(void *p_data, unsigned int idr_pic_id, unsigned int frame_seqno_in_gop, int *tail_nb_bits, u8 *tail);
