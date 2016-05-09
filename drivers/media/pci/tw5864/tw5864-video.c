fd52f09153adeb910ee71972896e82d0f618c965
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2016-05-09 17:07:58 +0300

    nit

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -213,8 +213,7 @@ static int tw5864_enable_input(struct tw5864_input *input)
 	tw_mask_shift_writel(TW5864_H264EN_CH_FMT_REG1, 0x3, 2 * input_number,
 			     fmt_reg_value);
 
-	tw_mask_shift_writel(
-			     (input_number < 2
+	tw_mask_shift_writel((input_number < 2
 			      ? TW5864_H264EN_RATE_MAX_LINE_REG1
 			      : TW5864_H264EN_RATE_MAX_LINE_REG2),
 			     0x1f, 5 * (input_number % 2),
