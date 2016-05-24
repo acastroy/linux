5abf13f572600007d1a9209b01e92b6773e20c6d
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2016-05-24 18:50:36 +0300

    Add tw5864 driver
    Support for boards based on Techwell TW5864 chip which provides
    multichannel video & audio grabbing and encoding (H.264, MJPEG,
    ADPCM G.726).
    
    Thanks to Mark Thompson <sw@jkqxz.net> for analysis of output video, and
    contribution of H.264 startcode emulation prevention.
    
    Signed-off-by: Andrey Utkin <andrey.utkin@corp.bluecherry.net>
    Tested-by: Andrey Utkin <andrey.utkin@corp.bluecherry.net>

---
+++ drivers/media/pci/tw5864/tw5864-config.c
@@ -0,0 +1,333 @@
+/*
+ *  TW5864 driver - analog decoders configuration functions
+ *
+ *  Copyright (C) 2015 Bluecherry, LLC <maintainers@bluecherrydvr.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#include "tw5864.h"
+#include "tw5864-reg.h"
+
+#define TW5864_IIC_RETRIES  30000
+
+/* Data written to registers */
+static u8 tbl_pal_tw2864_common[] = {
+	0x00, 0x00, 0x64, 0x11, 0x80, 0x80, 0x00, 0x12,
+	0x12, 0x20, 0x0a, 0xD0, 0x00, 0x00, 0x07, 0x7F,
+};
+
+static u8 tbl_ntsc_tw2864_common[] = {
+	0x00, 0x00, 0x64, 0x11, 0x80, 0x80, 0x00, 0x02,
+	0x12, 0xF0, 0x0C, 0xD0, 0x00, 0x00, 0x07, 0x7F,
+};
+
+static u8 tbl_pal_tw2864_common2[] = {
+	0x00, 0x22, 0x00, 0x00, 0x22, 0x00, 0x00, 0x22,
+	0x00, 0x00, 0x22, 0x00,
+};
+
+static u8 tbl_pal_tw2865_common[] = {
+	0x00, 0x00, 0x64, 0x11, 0x80, 0x80, 0x00, 0x12,
+	0x17, 0x20, 0x0C, 0xD0, 0x00, 0x00, 0x07, 0x7F,
+};
+
+static u8 tbl_ntsc_tw2865_common[] = {
+	0x00, 0x00, 0x64, 0x11, 0x80, 0x80, 0x00, 0x02,
+	0x12, 0xF0, 0x0C, 0xD0, 0x00, 0x00, 0x07, 0x7F,
+};
+
+static u8 audio_tw2865_common[] = {
+	0x33, 0x33, 0x03, 0x31, 0x75, 0xb9, 0xfd, 0x20,
+	0x64, 0xa8, 0xec, 0xC1, 0x00, 0x00, 0x00, 0x80,
+	0x00, 0xC0, 0xAA, 0xAA,
+};
+
+static u8 audio_tbl_pal_tw2865_8KHz[] = {
+	0x83, 0xB5, 0x09, 0x00, 0xA0, 0x00,
+};
+
+static u8 audio_tbl_pal_tw2865_16KHz[] = {
+	0x07, 0x6B, 0x13, 0x00, 0x40, 0x01,
+};
+
+static u8 audio_tbl_ntsc_tw2865_8KHz[] = {
+	0x83, 0xB5, 0x09, 0x78, 0x85, 0x00,
+};
+
+static u8 audio_tbl_ntsc_tw2865_16KHz[] = {
+	0x07, 0x6B, 0x13, 0xEF, 0x0A, 0x01,
+};
+
+/* Scatter-alike format: (addr) (value) */
+static u8 tbl_tw2864_other[] = {
+	0x89, 0x02,
+	0x94, 0x14,
+	0x9c, 0x20,
+	0x9e, 0x72,
+	0x9e, 0x7a,
+	0x9f, 0x00,
+	0xaa, 0x00,
+	0xb1, 0x2a,
+	0xca, 0xaa,
+	0xcb, 0x00,
+	0xcf, 0x83,
+	0xcf, 0x83,
+	0xd2, 0x01,
+	0xdb, 0xc1,
+	0xdd, 0x00,
+	0xde, 0x00,
+	0xe0, 0x00,
+	0xe1, 0xc0,
+	0xe2, 0xaa,
+	0xe3, 0xaa,
+	0xf8, 0x64,
+	0xf9, 0x11,
+	0xfa, 0xc6,
+	0xfb, 0x6f,
+	0xfc, 0xff,
+};
+
+static u8 tbl_tw2865_other1[] = {
+	0x18, 0x19,
+	0x1a, 0x06,
+	0x28, 0x19,
+	0x2a, 0x06,
+	0x38, 0x19,
+	0x3a, 0x06,
+	0x41, 0xd4,
+	0x43, 0x08,
+	0x60, 0x15,
+	0x61, 0x02,
+	0x6b, 0x0f,
+	0x6c, 0x0f,
+	0x96, 0xe6,
+	0x97, 0xc3,
+	0x9c, 0x20,
+	0x9e, 0x72,
+	0x9e, 0x7a,
+	0x9f, 0x03,
+	0xaa, 0x00,
+	0xb1, 0x2a,
+	0xca, 0x02,
+	0xf9, 0x51,
+	0xfa, 0x4a,
+	0xfb, 0x6f,
+	0xfc, 0xff,
+};
+
+static u8 tbl_tw2866_other1[] = {
+	0x08, 0x19,
+	0x0a, 0x06,
+	0x18, 0x19,
+	0x1a, 0x06,
+	0x28, 0x19,
+	0x2a, 0x06,
+	0x38, 0x19,
+	0x3a, 0x06,
+	0x41, 0xd4,
+	0x43, 0x08,
+	0x5b, 0xff,
+	0x60, 0x15,
+	0x61, 0x02,
+	0x6b, 0x0f,
+	0x6c, 0x0f,
+	0x96, 0xe6,
+	0x97, 0xc3,
+	0x9c, 0x20,
+	0x9e, 0x72,
+	0x9e, 0x7a,
+	0x9f, 0x00,
+	0xaa, 0x00,
+	0xb1, 0x2a,
+	0xca, 0x02,
+	0xf9, 0x51,
+	0xfa, 0x4a,
+	0xfb, 0x6f,
+	0xfc, 0xff,
+};
+
+static u8 tbl_tw2865_other2[] = {
+	0x70, 0x08,
+	0x73, 0x01,
+	0x7f, 0x80,
+	0xcf, 0x80,
+	0xf8, 0xc4,
+	0xf9, 0x51,
+};
+
+static u8 tbl_tw2865_other3[] = {
+	0x7e, 0xc0,
+	0x89, 0x05,
+	0xe0, 0x00,
+};
+
+static int i2c_read(struct tw5864_dev *dev, u8 devid, u8 addr, u8 *buf)
+{
+	u32 val;
+	int retries = TW5864_IIC_RETRIES;
+	unsigned long flags;
+
+	local_irq_save(flags);
+	tw_writel(TW5864_IIC, BIT(24) | (devid | 0x01) << 16 | addr << 8);
+	do {
+		val = tw_readl(TW5864_IIC);
+	} while (!(val & BIT(24)) && --retries);
+	local_irq_restore(flags);
+
+	if (!retries) {
+		dev_err(&dev->pci->dev,
+			"i2c_read() out of attempts on dev 0x%x, fn 0x%x\n",
+			devid, addr);
+		return -ETIMEDOUT;
+	}
+
+	*buf = (u8) val;
+	return 0;
+}
+
+static int i2c_write(struct tw5864_dev *dev, u8 devid, u8 addr, u8 buf)
+{
+	int retries = TW5864_IIC_RETRIES;
+	unsigned long flags;
+
+	local_irq_save(flags);
+	tw_writel(TW5864_IIC, BIT(24) | (devid & 0xfe) << 16 | addr << 8 | buf);
+	while (!(tw_readl(TW5864_IIC) & BIT(24)) && --retries)
+		;
+	local_irq_restore(flags);
+
+	if (!retries) {
+		dev_err(&dev->pci->dev,
+			"i2c_write() out of attempts on dev 0x%x, fn 0x%x\n",
+			devid, addr);
+		return -ETIMEDOUT;
+	}
+	return 0;
+}
+
+static int i2c_multi_write(struct tw5864_dev *dev, u8 devid, u8 addr, u8 *buf,
+			   u32 count)
+{
+	int i;
+	int ret;
+
+	for (i = 0; i < count; i++) {
+		ret = i2c_write(dev, devid, addr + i, buf[i]);
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
+
+static int i2c_wscatter(struct tw5864_dev *dev, u8 devid, u8 *buf, u32 count)
+{
+	int i;
+	u32 ret;
+
+	for (i = 0; i < count; i++) {
+		ret = i2c_write(dev, devid, buf[2 * i], buf[2 * i + 1]);
+		if (ret)
+			return ret;
+	}
+	return 0;
+}
+
+static int tw28xx_clkp_delay(struct tw5864_dev *dev, u8 devid, u32 base_ch)
+{
+	int delay;
+	u8 flags = 0;
+
+	i2c_read(dev, devid, 0x9f, &flags);
+	for (delay = 0; delay < 16; delay++) {
+		i2c_write(dev, devid, 0x9f, delay);
+		tw_writel(TW5864_H264EN_BUS0_MAP, base_ch);
+		/* clear error flags */
+		tw_writel(TW5864_UNDEFINED_ERROR_FLAGS_0x9218, 0x1);
+		mdelay(100);
+		if (tw_readl(TW5864_UNDEFINED_ERROR_FLAGS_0x9218) == 0)
+			break;
+	}
+	i2c_write(dev, devid, 0x9f, flags);
+
+	if (delay >= 16) {
+		dev_err(&dev->pci->dev,
+			"Cannot find suitable clkp_del for devid 0x%02x\n",
+			devid);
+		return -EFAULT;
+	}
+
+	dev_dbg(&dev->pci->dev, "Auto detect CLKP_DEL = %02x\n", delay);
+	return 0;
+}
+
+static int init_tw2864(struct tw5864_dev *dev, int index)
+{
+	u8 devid = 0x52 + index * 2;
+	int clkp_delay_check_chan = (index + 1) * 4;
+	u32 ch;
+	int ret = 0;
+
+	for (ch = 0; ch < 4; ch++)
+		ret |= i2c_multi_write(dev, devid, ch * 0x10,
+				      tbl_pal_tw2864_common,
+				      sizeof(tbl_pal_tw2864_common));
+
+	ret |= i2c_wscatter(dev, devid, tbl_tw2864_other,
+		     sizeof(tbl_tw2864_other) / 2);
+
+	if (ret)
+		return ret;
+
+	return tw28xx_clkp_delay(dev, devid, clkp_delay_check_chan);
+}
+
+static int init_tw2865(struct tw5864_dev *dev, u8 devid)
+{
+	u32 ch;
+	int ret = 0;
+
+	for (ch = 0; ch < 4; ch++) {
+		ret |= i2c_multi_write(dev, devid, ch * 0x10,
+				      tbl_pal_tw2865_common,
+				      sizeof(tbl_pal_tw2865_common));
+	}
+
+	ret |= i2c_wscatter(dev, devid, tbl_tw2865_other1,
+		     sizeof(tbl_tw2865_other1) / 2);
+	ret |= i2c_multi_write(dev, devid, 0xd0, audio_tw2865_common,
+			sizeof(audio_tw2865_common));
+	ret |= i2c_wscatter(dev, devid, tbl_tw2865_other2,
+		     sizeof(tbl_tw2865_other2) / 2);
+	ret |= i2c_multi_write(dev, devid, 0xf0, audio_tbl_pal_tw2865_8KHz,
+			sizeof(audio_tbl_pal_tw2865_8KHz));
+	ret |= i2c_wscatter(dev, devid, tbl_tw2865_other3,
+		     sizeof(tbl_tw2865_other3) / 2);
+	ret |= i2c_write(dev, devid, 0xe0, 0x10);
+
+	return ret;
+}
+
+int tw5864_init_ad(struct tw5864_dev *dev)
+{
+	int i;
+	int ret;
+
+	tw_writel(TW5864_IIC_ENB, 1);
+	tw_writel(TW5864_I2C_PHASE_CFG, 1);
+
+	for (i = 0; i < 3; i++) {
+		ret = init_tw2864(dev, i);
+		if (ret)
+			return ret;
+	}
+	return init_tw2865(dev, 0x50);
+}
---
+++ drivers/media/pci/tw5864/tw5864-h264.c
@@ -0,0 +1,175 @@
+/*
+ *  TW5864 driver - H.264 headers generation functions
+ *
+ *  Copyright (C) 2015 Bluecherry, LLC <maintainers@bluecherrydvr.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+#include "tw5864.h"
+#include "tw5864-bs.h"
+
+static u8 marker[] = { 0x00, 0x00, 0x00, 0x01 };
+
+/* log2 of max GOP size, taken 8 as V4L2-advertised max GOP size is 255 */
+#define LOG2_MAX_FRAME_NUM 8
+#define LOG2_MAX_POC_LSB LOG2_MAX_FRAME_NUM
+
+static int tw5864_h264_gen_sps_rbsp(u8 *buf, size_t size, int width, int height)
+{
+	struct bs bs, *s;
+
+	s = &bs;
+	bs_init(s, buf, size);
+	bs_write(s, 8, 0x42); /* profile_idc, baseline */
+	bs_write(s, 1, 1); /* constraint_set0_flag */
+	bs_write(s, 1, 1); /* constraint_set1_flag */
+	bs_write(s, 1, 0); /* constraint_set2_flag */
+	bs_write(s, 5, 0); /* reserved_zero_5bits */
+	bs_write(s, 8, 0x1E); /* level_idc */
+	bs_write_ue(s, 0); /* seq_parameter_set_id */
+	bs_write_ue(s, LOG2_MAX_FRAME_NUM - 4); /* log2_max_frame_num_minus4 */
+	bs_write_ue(s, 0); /* pic_order_cnt_type */
+	/* log2_max_pic_order_cnt_lsb_minus4 */
+	bs_write_ue(s, LOG2_MAX_POC_LSB - 4);
+	bs_write_ue(s, 1); /* num_ref_frames */
+	bs_write(s, 1, 0); /* gaps_in_frame_num_value_allowed_flag */
+	bs_write_ue(s, width / 16 - 1); /* pic_width_in_mbs_minus1 */
+	bs_write_ue(s, height / 16 - 1); /* pic_height_in_map_units_minus1 */
+	bs_write(s, 1, 1); /* frame_mbs_only_flag */
+	bs_write(s, 1, 0); /* direct_8x8_inference_flag */
+	bs_write(s, 1, 0); /* frame_cropping_flag */
+	bs_write(s, 1, 0); /* vui_parameters_present_flag */
+	bs_rbsp_trailing(s);
+	return bs_len(s);
+}
+
+static int tw5864_h264_gen_pps_rbsp(u8 *buf, size_t size, int qp)
+{
+	struct bs bs, *s;
+
+	s = &bs;
+	bs_init(s, buf, size);
+	bs_write_ue(s, 0); /* pic_parameter_set_id */
+	bs_write_ue(s, 0); /* seq_parameter_set_id */
+	bs_write(s, 1, 0); /* entropy_coding_mode_flag */
+	bs_write(s, 1, 0); /* pic_order_present_flag */
+	bs_write_ue(s, 0); /* num_slice_groups_minus1 */
+	bs_write_ue(s, 0); /* i_num_ref_idx_l0_active_minus1 */
+	bs_write_ue(s, 0); /* i_num_ref_idx_l1_active_minus1 */
+	bs_write(s, 1, 0); /* weighted_pred_flag */
+	bs_write(s, 2, 0); /* weighted_bipred_idc */
+	bs_write_se(s, qp - 26); /* pic_init_qp_minus26 */
+	bs_write_se(s, qp - 26); /* pic_init_qs_minus26 */
+	bs_write_se(s, 0); /* chroma_qp_index_offset */
+	bs_write(s, 1, 0); /* deblocking_filter_control_present_flag */
+	bs_write(s, 1, 0); /* constrained_intra_pred_flag */
+	bs_write(s, 1, 0); /* redundant_pic_cnt_present_flag */
+	bs_rbsp_trailing(s);
+	return bs_len(s);
+}
+
+static int tw5864_h264_gen_slice_head(u8 *buf, size_t size,
+				      unsigned int idr_pic_id,
+				      unsigned int frame_seqno_in_gop,
+				      int *tail_nb_bits, u8 *tail)
+{
+	struct bs bs, *s;
+	int is_i_frame = frame_seqno_in_gop == 0;
+
+	s = &bs;
+	bs_init(s, buf, size);
+	bs_write_ue(s, 0); /* first_mb_in_slice */
+	bs_write_ue(s, is_i_frame ? 2 : 5); /* slice_type - I or P */
+	bs_write_ue(s, 0); /* pic_parameter_set_id */
+	bs_write(s, LOG2_MAX_FRAME_NUM, frame_seqno_in_gop); /* frame_num */
+	if (is_i_frame)
+		bs_write_ue(s, idr_pic_id);
+
+	/* pic_order_cnt_lsb */
+	bs_write(s, LOG2_MAX_POC_LSB, frame_seqno_in_gop);
+
+	if (is_i_frame) {
+		bs_write1(s, 0); /* no_output_of_prior_pics_flag */
+		bs_write1(s, 0); /* long_term_reference_flag */
+	} else {
+		bs_write1(s, 0); /* num_ref_idx_active_override_flag */
+		bs_write1(s, 0); /* ref_pic_list_reordering_flag_l0 */
+		bs_write1(s, 0); /* adaptive_ref_pic_marking_mode_flag */
+	}
+
+	bs_write_se(s, 0); /* slice_qp_delta */
+
+	if (s->bits_left != 8) {
+		*tail = ((s->ptr[0]) << s->bits_left);
+		*tail_nb_bits = 8 - s->bits_left;
+	} else {
+		*tail = 0;
+		*tail_nb_bits = 0;
+	}
+
+	return bs_len(s);
+}
+
+void tw5864_h264_put_stream_header(u8 **buf, size_t *space_left, int qp,
+				   int width, int height)
+{
+	int nal_len;
+
+	/* SPS */
+	memcpy(*buf, marker, sizeof(marker));
+	*buf += 4;
+	*space_left -= 4;
+
+	**buf = 0x67; /* SPS NAL header */
+	*buf += 1;
+	*space_left -= 1;
+
+	nal_len = tw5864_h264_gen_sps_rbsp(*buf, *space_left, width, height);
+	*buf += nal_len;
+	*space_left -= nal_len;
+
+	/* PPS */
+	memcpy(*buf, marker, sizeof(marker));
+	*buf += 4;
+	*space_left -= 4;
+
+	**buf = 0x68; /* PPS NAL header */
+	*buf += 1;
+	*space_left -= 1;
+
+	nal_len = tw5864_h264_gen_pps_rbsp(*buf, *space_left, qp);
+	*buf += nal_len;
+	*space_left -= nal_len;
+}
+
+void tw5864_h264_put_slice_header(u8 **buf, size_t *space_left,
+				  unsigned int idr_pic_id,
+				  unsigned int frame_seqno_in_gop,
+				  int *tail_nb_bits, u8 *tail)
+{
+	int nal_len;
+
+	memcpy(*buf, marker, sizeof(marker));
+	*buf += 4;
+	*space_left -= 4;
+
+	/* Frame NAL header */
+	**buf = (frame_seqno_in_gop == 0) ? 0x25 : 0x21;
+	*buf += 1;
+	*space_left -= 1;
+
+	nal_len = tw5864_h264_gen_slice_head(*buf, *space_left, idr_pic_id,
+					     frame_seqno_in_gop, tail_nb_bits,
+					     tail);
+	*buf += nal_len;
+	*space_left -= nal_len;
+}
