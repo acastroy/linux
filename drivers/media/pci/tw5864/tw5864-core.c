199a09acb27c006c4433bc05214c9aca8717a84f
Andrey Utkin <andrey_utkin@fastmail.com>
2016-05-31 01:23:01 +0300

    Move indir r/w func to -util.c

--- drivers/media/pci/tw5864/tw5864-core.c
+++ drivers/media/pci/tw5864/tw5864-core.c
@@ -54,42 +54,6 @@ static const struct pci_device_id tw5864_pci_tbl[] = {
 	{0,}
 };
 
-void tw5864_indir_writeb(struct tw5864_dev *dev, u16 addr, u8 data)
-{
-	int retries = 30000;
-
-	while (tw_readl(TW5864_IND_CTL) & BIT(31) && --retries)
-		;
-	if (!retries)
-		dev_err(&dev->pci->dev,
-			"tw_indir_writel() retries exhausted before writing\n");
-
-	tw_writel(TW5864_IND_DATA, data);
-	tw_writel(TW5864_IND_CTL, addr << 2 | TW5864_RW | TW5864_ENABLE);
-}
-
-u8 tw5864_indir_readb(struct tw5864_dev *dev, u16 addr)
-{
-	int retries = 30000;
-
-	while (tw_readl(TW5864_IND_CTL) & BIT(31) && --retries)
-		;
-	if (!retries)
-		dev_err(&dev->pci->dev,
-			"tw_indir_readl() retries exhausted before reading\n");
-
-	tw_writel(TW5864_IND_CTL, addr << 2 | TW5864_ENABLE);
-
-	retries = 30000;
-	while (tw_readl(TW5864_IND_CTL) & BIT(31) && --retries)
-		;
-	if (!retries)
-		dev_err(&dev->pci->dev,
-			"tw_indir_readl() retries exhausted at reading\n");
-
-	return (u8) tw_readl(TW5864_IND_DATA);
-}
-
 void tw5864_irqmask_apply(struct tw5864_dev *dev)
 {
 	tw_writel(TW5864_INTR_ENABLE_L, dev->irqmask & 0xffff);
