8dcd3363451eb2a16dee63840a74335ad293ebfc
Andrey Utkin <andrey_utkin@fastmail.com>
2016-05-31 22:35:54 +0300

    Feed FPS control register closer to original driver

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -698,31 +698,28 @@ static void tw5864_frame_interval_set(struct tw5864_input *input)
 	 * frame is discarded.
 	 * So unary representation would work, but more or less equal gaps
 	 * between the frames should be preserved.
+	 *
 	 * For 1 FPS - 0x00000001
 	 * 00000000 00000000 00000000 00000001
 	 *
-	 * For 2 FPS - 0x00010001.
-	 * 00000000 00000001 00000000 00000001
+	 * For max FPS - set all 25/30 lower bits:
+	 * 00111111 11111111 11111111 11111111 (NTSC)
+	 * 00000001 11111111 11111111 11111111 (PAL)
 	 *
-	 * For 4 FPS - 0x01010101.
-	 * 00000001 00000001 00000001 00000001
-	 *
-	 * For 8 FPS - 0x11111111.
-	 * 00010001 00010001 00010001 00010001
-	 *
-	 * For 16 FPS - 0x55555555.
-	 * 01010101 01010101 01010101 01010101
-	 *
-	 * For 32 FPS (not reached - capped by 25/30 limit) - 0xffffffff.
-	 * 11111111 11111111 11111111 11111111
+	 * For half of max FPS - use such pattern:
+	 * 00010101 01010101 01010101 01010101 (NTSC)
+	 * 00000001 01010101 01010101 01010101 (PAL)
 	 *
 	 * Et cetera.
+	 *
+	 * The value supplied to hardware is capped by mask of 25/30 lower bits.
 	 */
 	struct tw5864_dev *dev = input->root;
 	u32 unary_framerate = 0;
 	int shift = 0;
+	int std_max_fps = input->std == STD_NTSC ? 30 : 25;
 
-	for (shift = 0; shift < 32; shift += input->frame_interval)
+	for (shift = 0; shift < std_max_fps; shift += input->frame_interval)
 		unary_framerate |= 0x00000001 << shift;
 
 	tw_writel(TW5864_H264EN_RATE_CNTL_LO_WORD(input->nr, 0),
