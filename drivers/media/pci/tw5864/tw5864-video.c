24d017f7e5299f2521c338cf877a2dc633561a6d
Andrey Utkin <andrey_utkin@fastmail.com>
2016-07-08 09:03:22 +0300

    Change container_of -> list_entry where applicable

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -446,8 +446,8 @@ static int tw5864_start_streaming(struct vb2_queue *q, unsigned int count)
 		return 0;
 
 	while (!list_empty(&input->active)) {
-		struct tw5864_buf *buf = container_of(input->active.next,
-						      struct tw5864_buf, list);
+		struct tw5864_buf *buf = list_entry(input->active.next,
+						    struct tw5864_buf, list);
 
 		list_del(&buf->list);
 		vb2_buffer_done(&buf->vb.vb2_buf, VB2_BUF_STATE_QUEUED);
@@ -468,8 +468,8 @@ static void tw5864_stop_streaming(struct vb2_queue *q)
 		input->vb = NULL;
 	}
 	while (!list_empty(&input->active)) {
-		struct tw5864_buf *buf = container_of(input->active.next,
-						      struct tw5864_buf, list);
+		struct tw5864_buf *buf = list_entry(input->active.next,
+						    struct tw5864_buf, list);
 
 		list_del(&buf->list);
 		vb2_buffer_done(&buf->vb.vb2_buf, VB2_BUF_STATE_ERROR);
