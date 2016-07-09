0792a075be15d135a3ffe345f32d5845315f3cc9
Andrey Utkin <andrey_utkin@fastmail.com>
2016-07-09 15:21:59 +0300

    Finish reworking frameintervals

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -722,14 +722,9 @@ static void tw5864_frame_interval_set(struct tw5864_input *input)
 static int tw5864_frameinterval_get(struct tw5864_input *input,
 				    struct v4l2_fract *frameinterval)
 {
-	int ret;
 	enum tw5864_vid_std std;
 
-	ret = tw5864_input_std_get(input, &std);
-	if (ret)
-		return ret;
-
-	switch (std) {
+	switch (input->std) {
 	case STD_NTSC:
 		frameinterval->numerator = 1001;
 		frameinterval->denominator = 30000;
@@ -769,17 +764,29 @@ static int tw5864_enum_frameintervals(struct file *file, void *priv,
 				      struct v4l2_frmivalenum *fintv)
 {
 	struct tw5864_input *input = video_drvdata(file);
+	struct v4l2_fract frameinterval;
+	int std_max_fps = input->std == STD_NTSC ? 30 : 25;
+	struct v4l2_frmsizeenum fsize = { .index = fintv->index,
+		.pixel_format = fintv->pixel_format };
+	int ret;
 
-	if (fintv->pixel_format != V4L2_PIX_FMT_H264)
-		return -EINVAL;
-	if (fintv->index)
-		return -EINVAL;
-	if (fintv->width != input->width || fintv->height != input->height)
+	ret = tw5864_enum_framesizes(file, priv, &fsize);
+	if (ret)
+		return ret;
+
+	if (fintv->width != fsize.discrete.width ||
+	    fintv->height != fsize.discrete.height)
 		return -EINVAL;
 
 	fintv->type = V4L2_FRMIVAL_TYPE_STEPWISE;
 
-	return tw5864_frameinterval_get(input, &fintv->discrete);
+	ret = tw5864_frameinterval_get(input, &frameinterval);
+	fintv->stepwise.step = frameinterval;
+	fintv->stepwise.min = frameinterval;
+	fintv->stepwise.max = frameinterval;
+	fintv->stepwise.max.numerator *= std_max_fps;
+
+	return ret;
 }
 
 static int tw5864_g_parm(struct file *file, void *priv,
