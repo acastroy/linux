18a88d5a029c682db3f484a3931dc38c817383e9
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2016-07-11 14:12:42 +0300

    whitespace fix

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -635,6 +635,7 @@ static int tw5864_querystd(struct file *file, void *priv, v4l2_std_id *std)
 static int tw5864_g_std(struct file *file, void *priv, v4l2_std_id *std)
 {
 	struct tw5864_input *input = video_drvdata(file);
+
 	*std = input->v4l2_std;
 	return 0;
 }
