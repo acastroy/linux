adeddf202b50add57e5436a351cd5a48d6327da8
Andrey Utkin <andrey.utkin@corp.bluecherry.net>
2016-07-20 15:38:54 +0300

    Change tw5864_input_std_get() behaviour as suggested by Hans, and also simplify

--- drivers/media/pci/tw5864/tw5864-video.c
+++ drivers/media/pci/tw5864/tw5864-video.c
@@ -215,26 +215,19 @@ static void tw5864_buf_queue(struct vb2_buffer *vb)
 }
 
 static int tw5864_input_std_get(struct tw5864_input *input,
-				enum tw5864_vid_std *std_arg)
+				enum tw5864_vid_std *std)
 {
 	struct tw5864_dev *dev = input->root;
-	enum tw5864_vid_std std;
 	u8 std_reg = tw_indir_readb(TW5864_INDIR_VIN_E(input->nr));
 
-	std = (std_reg & 0x70) >> 4;
+	*std = (std_reg & 0x70) >> 4;
 
 	if (std_reg & 0x80) {
-		dev_err(&dev->pci->dev,
+		dev_dbg(&dev->pci->dev,
 			"Video format detection is in progress, please wait\n");
 		return -EAGAIN;
 	}
 
-	if (std == STD_INVALID) {
-		dev_err(&dev->pci->dev, "No valid video format detected\n");
-		return -EPERM;
-	}
-
-	*std_arg = std;
 	return 0;
 }
 
